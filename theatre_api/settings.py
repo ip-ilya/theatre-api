7aaa476151e49292c71d7c1c0e088e62904bcc09
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-05-02 15:50:34 +0300

    update installed_apps and define AUTH_USER_MODEL

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "theatre",
+    "user"
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
