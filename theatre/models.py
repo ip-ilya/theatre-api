57724be6ddc16101165d096cf23efe3f79b20c00
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-05-02 15:50:58 +0300

    create models

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+from theatre_api import settings
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="plays")
+    genres = models.ManyToManyField(Actor, related_name="plays")
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=64)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(
+        Play,
+        on_delete=models.CASCADE,
+        related_name="performances"
+    )
+    theatre_hall = models.ForeignKey(
+        TheatreHall,
+        on_delete=models.CASCADE,
+        related_name="performances"
+    )
+    show_time = models.DateTimeField()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reservations"
+    )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey(
+        Performance,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
