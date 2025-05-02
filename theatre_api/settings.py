bbfc07a8ce67e9ef9c02c48e4a1f6faba137a19a
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-05-02 16:07:12 +0300

    add rest_framework to installed_apps

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework"
     "theatre",
-    "user"
+    "user",
 ]
 
 AUTH_USER_MODEL = "user.User"
