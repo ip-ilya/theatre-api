5f48568d574b5e1cf8dbeb63c1247df83771987e
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-05-02 16:12:24 +0300

    add admin to installed_apps & update urls

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework"
+    "django.contrib.admin"
+    "rest_framework",
     "theatre",
     "user",
 ]
